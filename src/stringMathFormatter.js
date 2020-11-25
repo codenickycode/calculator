7164edf888eda3dfec47a0b48c214b1ab2c5034c
nfcarbone <nfcarbone@gmail.com>
2020-11-25 06:16:18 -0500

    stringMathFormatter errors

--- src/stringMathFormatter.js
+++ src/stringMathFormatter.js
@@ -1,8 +1,8 @@
 function toExpression(string) {
   string = string
     .replace(/[^0-9eE\.\+\-\*\/]/g, '') // remove anything that isn't arithmetic
-    .replace(/^\D*(?=\-\.\d)|^\D*(?=\.\d)|^\D*(?=\-\d)|^\D*(?=\d)/, '') // remove invalid leading characters
-    .replace(/\D+$/, '') // remove invalid trailing characters
+    .replace(/\.{2,}/g, '.') // replace multiple decimals with single decimal
+    .replace(/^\D*(?=\-\.\d)|^\D*(?=\.\d)|^\D*(?=\-\d)|^\D*(?=\d)|\D+$/, '') // remove invalid leading/trailing characters
     .replace(/[\+\-\*\/]*(\+\-|\-\-|\*\-|\/\-)/g, '$1') // replace 2+ operators but preserve negative signs
     .replace(/[\+\-\*\/]+([\+\*\/])/g, '$1') // replace remaining 2+ operators
     .replace(/((\d\.\d+)(?:\.+\d+)+|\.+(\D))/g, '$2'); // truncate everything after additional decimal
